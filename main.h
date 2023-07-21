28818b743850f71c0a9f807ab434d6456aa37d3c
mmgmolotov <mmgmolotov@gmail.com>
2023-07-21 10:12:09 -0700

    create header file 'main.h'

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+
+
+#endif
