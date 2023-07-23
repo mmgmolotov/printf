d6655574735bee5142b97d467a94524936cb67c4
mmgmolotov <mmgmolotov@gmail.com>
2023-07-23 10:12:33 -0700

    add betty doc to struct

--- main.h
+++ main.h
@@ -13,6 +13,12 @@ void print_str(va_list args);
 void print_char(va_list args);
 void print_perc(va_list args);
 
+/**
+ * struct - for handling format
+ * @spc: format specifier
+ * @hndl: handler
+ */
+
 typedef struct {
 	char spc;
 	void (*hndl)(va_list);
