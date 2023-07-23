0873f34029cc1850d0818c304c5c93a85c8b325e
Mouad-marah <mouadmarah0@gmail.com>
2023-07-23 06:34:42 -0700

    update fun.c and header

--- main.h
+++ main.h
@@ -9,12 +9,13 @@
 #include <stdio.h>
 
 int _printf(const char *format, ...);
+void print_str(va_list args);
+void print_char(va_list args);
+void print_perc(va_list args);
 
 typedef struct {
 	char spc;
 	void (*hnd)(va_list);
-} fh;
-
-
+} FH;
 
 #endif
