34b01db98b05f34874e202a7adcb04e60b24a00a
mmgmolotov <mmgmolotov@gmail.com>
2023-07-23 11:27:28 -0700

    restore previous version

--- _printf.c
+++ _printf.c
@@ -10,47 +10,38 @@ int _printf(const char *format, ...)
 		{'%', print_perc}, {'\0', NULL} };
 	va_list args;
 	int counter, i;
-	char c;
 
 	va_start(args, format);
 	counter = 0;
-	while ((c == *format++))
+	while (*format != '\0')
 	{
-		if (c == '%')
+		if (*format == '%')
 		{
-			c = *format++;
-			if (c == '%')
+			format++;
+			i = 0;
+			while (def[i].spc != '\0')
+			{
+				if (*format == def[i].spc)
+				{
+					def[i].hndl(args);
+					counter++;
+					break;
+				}
+				i++;
+			}
+			if (def[i].spc == '\0')
 			{
 				putchar('%');
-				counter++;
+				putchar(*format);
+				counter += 2;
 			}
-			else
-			{
-				i = 0;
-				while (def[i].spc != '\0')
-				{
-					if (*format == def[i].spc)
-					{
-						def[i].hndl(args);
-						counter++;
-						break;
-					}
-					i++;
-				}
-				if (def[i].spc == '\0')
-				{
-					putchar('%');
-					putchar(*format);
-					counter += 2;
-				}
-			}
-		}	
+		}
 		else
 		{
 			putchar(*format);
 			counter++;
 		}
-		
+		format++;
 	}
 	va_end(args);
 	return (counter);
