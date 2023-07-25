c18ff1a1b2304fd659cbd533953e8815d00a721a
Mouad-marah <mouadmarah0@gmail.com>
2023-07-25 16:32:51 -0700

    print reverse string

--- print_rev_string.c
+++ print_rev_string.c
@@ -9,7 +9,7 @@
 int print_rev(va_list args)
 {
 	int i, len = 0;
-	char temp_str[20], temp, *str;
+	char *str;
 
 	str = va_arg(args, char *);
 	if (!str)
@@ -28,17 +28,8 @@ int print_rev(va_list args)
 		str++;
 		len++;
 	}
-	for (i = 0; i <= len; i++)
-		temp_str[i] = str[i];
-	temp_str[i] = '\0';
-	for (i = 0; i < len / 2; i++)
-	{
-		temp = temp_str[i];
-		temp_str[i] = temp_str[len - i - 1];
-		temp_str[len - i - 1] = temp;
-	}
-	for (i = 0; temp_str[i] != 0 && i < len; i++)
-		putchar(temp_str[i]);
+	for (i = len - 1; i >= 0; i--)
+		putchar(str[i]);
 	}
 	return (len);
 }
